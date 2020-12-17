44712f337402194eb5cd5b987027762311970200
khoironi2 <khoironi7887@students.amikom.ac.id>
2020-12-17 19:50:19 +0700

    desc

--- application/models/Penjualan_model.php
+++ application/models/Penjualan_model.php
@@ -7,7 +7,8 @@ class Penjualan_model extends CI_Model
         $query = "SELECT `tbl_penjualan`.*, `tbl_users`.`name`, `tbl_katalog`.`nama_katalog`
         FROM `tbl_penjualan`
         INNER JOIN `tbl_users` ON `tbl_penjualan`.`id_users` = `tbl_users`.`id_users`
-        INNER JOIN `tbl_katalog` ON `tbl_penjualan`.`id_katalog` = `tbl_katalog`.`id_katalog`";
+        INNER JOIN `tbl_katalog` ON `tbl_penjualan`.`id_katalog` = `tbl_katalog`.`id_katalog`
+        ORDER BY id_katalog DESC";
 
         return $this->db->query($query)->result_array();
     }
