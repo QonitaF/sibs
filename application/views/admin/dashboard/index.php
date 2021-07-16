362a1db29ec19ffa1e0844e530e59c7ba46959f3
khoironi2 <khoironi7887@students.amikom.ac.id>
2021-07-16 13:13:26 +0700

    summernote

--- application/views/admin/dashboard/index.php
+++ application/views/admin/dashboard/index.php
@@ -1,12 +1,11 @@
 <h3 class="text-center">Selamat Datang Admin</h3>
 <?php foreach ($abouts as $about) : ?>
-<h3 class="text-center mt-3"><?= $about["nama_about"] ?></h3>
-<a class="btn btn-dark" href="<?= base_url('admin/edit_about/' . $about["id_about"]); ?>">Update</a>
-<?= $this->session->flashdata('message'); ?>
-
-<div class="card mt-5" >
-    <div class="card-body" style="margin-bottom: 450px;">
-        <?= $about["keterangan"]; ?>
+    <h3 class="text-center mt-3"><?= $about["nama_about"] ?></h3>
+    <a class="btn btn-dark" href="<?= base_url('admin/edit_about/' . $about["id_about"]); ?>">Update</a>
+    <?= $this->session->flashdata('message'); ?>
+    <div class="card mt-5">
+        <div class="card-body" style="margin-bottom: 450px;">
+            <p><?= $about["keterangan"]; ?></p>
+        </div>
     </div>
-</div>
 <?php endforeach; ?>
